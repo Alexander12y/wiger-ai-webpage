58e7f3ddb3ffdab25dee2e1eea97bd9c6473681d
erickclempner <erickclempner@proton.me>
2026-03-04 13:22:06 -0600

    agregar sercodam

--- src/components/sections/ClientLogosSection.tsx
+++ src/components/sections/ClientLogosSection.tsx
@@ -2,12 +2,12 @@
 
 export function ClientLogosSection() {
   const clientLogos = [
-    { id: 1, name: 'Cliente 1' },
-    { id: 2, name: 'Cliente 2' },
-    { id: 3, name: 'Cliente 3' },
-    { id: 4, name: 'Cliente 4' },
-    { id: 5, name: 'Cliente 5' },
-    { id: 6, name: 'Cliente 6' },
+    { id: 1, name: 'Cliente 1', imagen:  "C:\\erick\\wiger-ai-webpage\\public\\LOGO SERCODAM.png"},
+    { id: 2, name: 'Cliente 2', imagen:  "C:\\erick\\wiger-ai-webpage\\public\\LOGO SERCODAM.png" },
+    { id: 3, name: 'Cliente 3', imagen:  "C:\\erick\\wiger-ai-webpage\\public\\LOGO SERCODAM.png" },
+    { id: 4, name: 'Cliente 4', imagen:  "C:\\erick\\wiger-ai-webpage\\public\\LOGO SERCODAM.png" },
+    { id: 5, name: 'Cliente 5', imagen:  "C:\\erick\\wiger-ai-webpage\\public\\LOGO SERCODAM.png" },
+    { id: 6, name: 'Cliente 6', imagen:  "C:\\erick\\wiger-ai-webpage\\public\\LOGO SERCODAM.png" },
   ]
 
   return (
@@ -41,7 +41,7 @@ export function ClientLogosSection() {
                 className="font-semibold text-sm"
                 style={{ color: '#5A6E84' }}
               >
-                {logo.name}
+                {logo.imagen}
               </span>
             </div>
           ))}
